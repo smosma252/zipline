145611b318a2dd506d8bfd0da1a377855f4ad799
smosma252 <74032722+smosma252@users.noreply.github.com>
2020-11-06 01:45:51 -0500

    Update benchmarks.py

--- zipline/data/benchmarks.py
+++ zipline/data/benchmarks.py
@@ -12,41 +12,66 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import logbook
+import numpy as np
 
 import pandas as pd
 
-log = logbook.Logger(__name__)
+import pandas_datareader.data as pd_reader
 
 
-def get_benchmark_returns_from_file(filelike):
+
+def get_benchmark_returns(symbol, first_date, last_date):
+
     """
-    Get a Series of benchmark returns from a file
+
+    Get a Series of benchmark returns from Yahoo associated with `symbol`.
+
+    Default is `SPY`.
+
+
 
     Parameters
+
     ----------
-    filelike : str or file-like object
-        Path to the benchmark file.
-        expected csv file format:
-        date,return
-        2020-01-02 00:00:00+00:00,0.01
-        2020-01-03 00:00:00+00:00,-0.02
+
+    symbol : str
+
+        Benchmark symbol for which we're getting the returns.
+
+
+
+    The data is provided by Yahoo Finance
 
     """
-    log.info("Reading benchmark returns from {}", filelike)
 
-    df = pd.read_csv(
-        filelike,
-        index_col=['date'],
-        parse_dates=['date'],
-    ).tz_localize('utc')
+    data = pd_reader.DataReader(
 
-    if 'return' not in df.columns:
-        raise ValueError("The column 'return' not found in the "
-                         "benchmark file \n"
-                         "Expected benchmark file format :\n"
-                         "date, return\n"
-                         "2020-01-02 00:00:00+00:00,0.01\n"
-                         "2020-01-03 00:00:00+00:00,-0.02\n")
+        symbol,
 
-    return df['return'].sort_index()
+        'yahoo',
+
+        first_date,
+
+        last_date
+
+    )
+
+
+
+    data = data['Close']
+
+
+
+    data[pd.Timestamp('2008-12-15')] = np.nan
+
+    data[pd.Timestamp('2009-08-11')] = np.nan
+
+    data[pd.Timestamp('2012-02-02')] = np.nan
+
+
+
+    data = data.fillna(method='ffill')
+
+
+
+    return data.sort_index().tz_localize('UTC').pct_change(1).iloc[1:]
